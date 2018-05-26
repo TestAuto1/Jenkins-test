fd35819dc428f3ea57ce4f60a97ed4214341bc6c
GamilM <GamilM@E0366482.internal.vodafone.com>
2018-05-26 15:26:52 +0200

    init

---
+++ Jenkins-Demo/src/test/java/Pack/TCs.java
@@ -0,0 +1,22 @@
+package Pack;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class TCs {
+	
+	@Test 
+	public void tc_001()
+	{
+		
+		assertTrue(true);
+	}
+	
+	@Test 
+	public void tc_002()
+	{
+		
+		assertTrue(true);
+	}
+
+}
